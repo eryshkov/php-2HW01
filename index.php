355866ff3bc6abeb79bdc0157a90526eb65d3173
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-06 19:31:19 +0300

    Db test is added

--- index.php
+++ index.php
@@ -5,12 +5,9 @@ use App\Models\User;
 
 require __DIR__ . '/autoload.php';
 
-$articles = Article::findAll();
-$user = User::findAll();
+//$articles = Article::findAll();
+//$user = User::findAll();
+//
+//var_dump($user);
+//var_dump($articles);
 
-var_dump($user);
-var_dump($articles);
-
-//$db = new \App\Db();
-//$data = $db->query('SELECT * FROM newsf WHERE id=:id', [':id' => 1], Article::class);
-//var_dump($data);

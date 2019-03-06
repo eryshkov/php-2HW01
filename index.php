d5e6f49d8ce39303b6f635a8a5321315cc50517f
Evgenii Ryshkov <eryshkov@gmail.com>
2019-03-06 20:11:54 +0300

    index.php is cleaned

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Models\Article;
-use App\Models\User;
-
 require __DIR__ . '/autoload.php';
 
-//$articles = Article::findAll();
-//$user = User::findAll();
-//
-//var_dump($user);
-//var_dump($articles);
+
 
